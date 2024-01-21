25701bf0aa87fa962b5571aa0a5d90f03978b345
Kristy Springman <kristyspringman@gmail.com>
2024-01-21 15:31:35 +0900

    String end with string

---
+++ stringEndsWith.js
@@ -0,0 +1,20 @@
+//return true if the string ends with the second argument
+//ie ('abc','bc) -> true
+//('abc, 'd') -> false
+
+function solution(str, ending) {
+    let answer = ""
+
+    for (let i = 0; i < str.length - ending.length; i++) {
+        answer += str[i]
+    }
+    answer += ending
+    if (answer === str) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(solution('abcde', 'cde'))
+console.log(solution('abcde', 'abc'))
