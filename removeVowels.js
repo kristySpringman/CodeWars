c7884c56b845ba16d6b2a321cf807dd2803e339a
Kristy Springman <kristyspringman@gmail.com>
2024-01-21 16:05:18 +0900

    removing vowels

---
+++ removeVowels.js
@@ -0,0 +1,13 @@
+//remove all vowels from strings
+
+function disemvowel(str) {
+    let answer = str.replace(/a|A|e|E|i|I|o|O|u|U/g, "")
+    return answer;
+}
+
+console.log(disemvowel("This website is for losers LOL!"))
+//"Ths wbst s fr lsrs LL!"
+console.log(disemvowel("No offense but,\nYour writing is among the worst I've ever read"))
+//"N ffns bt,\nYr wrtng s mng th wrst 'v vr rd"
+console.log(disemvowel("What are you, a communist?"))
+//"Wht r y,  cmmnst?"
